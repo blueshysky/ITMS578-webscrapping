b56ad6d1638d12af1013167495679910cd6fab6a
blueshysky <xionghuih745@gmail.com>
2019-11-09 17:25:33 -0600

    made a simple gui open window and label

--- index.py
+++ index.py
@@ -1,15 +1,28 @@
+from tkinter import *
 import requests
 import time
-import urllib3
+import urllib3.request
 from bs4 import BeautifulSoup
 
+#make a window
+Window = Tk()
+Window.geometry("300x300")
+Window.title("Webbsite")
+
+#make a label
+L1 = Label( Window, text="Welcome", fg='blue', bg='yellow', font=("arial", 16, "bold") ).pack()
+
+Window.mainloop()
+
 #grab url from a site
-url = 'https://www.hackthissite.org/pages/index/index.php'
+#url = 'https://www.google.com/'
 
 #test to see if there is a respond
-respond = requests.get(url)
-print(respond)
+# respond = requests.get(url)
+#print(respond)
 
-#This step is to 
-#soup = BeautifulSoup(html_doc, 'html.parser')
-# soup.findAll('a')
+#This step is to parse html to txt
+# soup = BeautifulSoup(respond.text, "html.parser")
+
+# print(soup.find_all('a'))
+
