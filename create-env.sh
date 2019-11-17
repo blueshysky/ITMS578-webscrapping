782a6cf3a305eb2c3caa6bebcc9a13bb15cac88b
stsang123412 <stsang@hawk.iit.edu>
2019-11-17 04:15:42 +0000

    updated the create-env.script to install the needed dependencies needed to run the .py file

--- create-env.sh
+++ create-env.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
-sudo apt-get install python3
-sudo apt-get install python-pip
-pip install requests
-pip install BeautifulSoup4
+sudo apt update 
+#sudo apt install python3 -y 
+
+echo Installing pip, tkinter, requests, and bs4
+sudo apt install python3-pip -y 
+sudo apt-get install python3-tk -y 
+sudo apt-get install python3-bs4 -y
+
+sudo pip3 install requests 
+sudo pip3 install BeautifulSoup4 
+sudo pip3 install BeautifulSoup4
+
